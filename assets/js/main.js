0196c0df546862f09915ad8cc49b9dcd78c28552
Fernando Cassio <fernandocassiodev@gmail.com>
2024-02-18 19:26:11 -0300

    add a modal with details of pokemons

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 const pokemonList = document.getElementById('pokemonList')
 const LoadMoreButton = document.getElementById('loadMoreButton')
 
-const maxRecords = 500
+const maxRecords = 1000
 const limit = 10
 let offset = 0;
 
@@ -13,18 +13,19 @@ function loadPokemonItens(offset, limit) {
 
     pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
             const newHtml = pokemons.map((pokemon) => 
-                `<li class="pokemon ${pokemon.type}">
-                <span class="number">#${pokemon.number}</span>
-                <span class="name">${pokemon.name}</span>
-            
-                <div class="detail">
-                    <ol class="types">
-                        ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
-                    </ol>
-                    <img class="imagens-pokemons" src="${pokemon.photo}" 
-                    alt="${pokemon.name}">
-                </div>
-            </li>
+                `<div class="pokemonSquare">
+                    <li class="pokemon ${pokemon.type}">
+                    <span class="number">#${pokemon.number}</span>
+                    <span class="name">${pokemon.name}</span>
+                    <div class="detail">
+                        <ol class="types">
+                            ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                        </ol>
+                        <img class="imagens-pokemons" src="${pokemon.photo}"
+                        alt="${pokemon.name}">
+                    </div>
+                    </li>
+            </div>
             `
             ).join('')
             pokemonList.innerHTML += newHtml
